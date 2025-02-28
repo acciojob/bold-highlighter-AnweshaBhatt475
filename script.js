c3a4eb65807b994af25c033fb6caa4322efa1cd7
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-02-28 23:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 let strongText = document.querySelectorAll("strong");
+
 function highlight() {
-    //Write your code here
-	for(let t of strongText){
-		t.style.color = rgb(0, 128, 0)
-	}
-	
-
-
+    // Write your code here
+    strongText.forEach(t => {
+        t.style.color = "rgb(0, 128, 0)";
+    });
 }
-
 
 function return_normal() {
-    //Write your code here
-for(let t of strongText){
-	t.style.color = rgb(0, 0, 0)
-}
-    
+    // Write your code here
+    strongText.forEach(t => {
+        t.style.color = "rgb(0, 0, 0)";
+    });
 }
